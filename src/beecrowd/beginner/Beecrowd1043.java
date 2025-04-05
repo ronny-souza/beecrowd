1556fc9e327e5624dbb9624073d883f3470c0535
ronny-souza <ronnymarinho2@gmail.com>
2025-04-05 17:04:22 -0300

    ✨ feat: resolução do problema 1043

---
+++ src/beecrowd/beginner/Beecrowd1043.java
@@ -0,0 +1,29 @@
+package beecrowd.beginner;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Beecrowd1043 {
+
+    /* https://judge.beecrowd.com/pt/problems/view/1042 */
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        boolean isTriangle = ((a < (b + c)) && (b < (a + c)) && (c < (a + b)));
+
+        if (isTriangle) {
+            double perimeter = a + b + c;
+            System.out.printf("Perimetro = %.1f%n", perimeter);
+        } else {
+            double area = ((a + b) * c) / 2;
+            System.out.printf("Area = %.1f%n", area);
+        }
+
+        scanner.close();
+    }
+}
