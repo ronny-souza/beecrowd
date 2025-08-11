6f4569cb201180fab06fcba9dc7344e4a4f705f2
ronyeri <ronyeri@usto.re>
2025-08-11 19:25:54 -0300

    ✨ feat: resolução do problema 1052

---
+++ src/beecrowd/beginner/Beecrowd1052.java
@@ -0,0 +1,35 @@
+package beecrowd.beginner;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Beecrowd1052 {
+
+    /* https://judge.beecrowd.com/pt/problems/view/1052 */
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+
+        int code = scanner.nextInt();
+
+        Map<Integer, String> months = Map.ofEntries(
+                Map.entry(1, "January"),
+                Map.entry(2, "February"),
+                Map.entry(3, "March"),
+                Map.entry(4, "April"),
+                Map.entry(5, "May"),
+                Map.entry(6, "June"),
+                Map.entry(7, "July"),
+                Map.entry(8, "August"),
+                Map.entry(9, "September"),
+                Map.entry(10, "October"),
+                Map.entry(11, "November"),
+                Map.entry(12, "December")
+        );
+
+        System.out.println(months.get(code));
+        scanner.close();
+    }
+}
